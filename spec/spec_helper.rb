b155c1ad5903888411fd739efe2517358024f8e9
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-11-01 10:30:30 +0300

    relocated the code to the right component

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,17 +96,4 @@ RSpec.configure do |config|
 =end
 end
 
-class Dog
-  def name=(dog_name)
-    @this_dogs_name = dog_name
-  end
 
-  def name
-    @this_dogs_name
-  end
-end
-
-lassie = Dog.new
-lassie.name = "Lassie"
-
-puts lassie.name
